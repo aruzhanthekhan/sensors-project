d3ecf569afca1fba9126047548cade7284566652
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2023-03-16 11:34:46 +0600

    update models and rename directory

--- flask-server/models/Admins.py
+++
@@ -1,5 +0,0 @@
-from app import db
-
-class Admins(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, nullable=False, index=True)
--- flask-server/models/Chairmans.py
+++
@@ -1,5 +0,0 @@
-from app import db
-
-class Chairmans(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, nullable=False, index=True)
