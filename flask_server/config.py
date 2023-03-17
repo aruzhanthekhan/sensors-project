a494a71ee780865919a210a50484393e85c4bcd7
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2023-03-17 12:51:21 +0600

    config and models are edited

--- flask_server/config.py
+++ flask_server/config.py
@@ -6,7 +6,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DB_URI = getenv('SQLALCHEMY_DB_URI')
+db_data = {
+    'user': getenv('DB_USERNAME'),
+    'password': getenv('DB_PASSWORD'),
+    'host': getenv('DB_HOST'),
+    'name': getenv('DB_NAME')
+}
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DB_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = getenv('SECRET_KEY')
 
 # engine = create_engine(SQLALCHEMY_DB_URI, future=True, pool_pre_ping=True)
 # Base = declarative_base()
