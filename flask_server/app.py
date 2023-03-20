4845d93a89ce653dd5c7b9181a40e5f6012ebc56
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2023-03-20 14:20:26 +0600

    perform postgresql db connection

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from config import SQLALCHEMY_DB_URI
+from config import Config
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DB_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
